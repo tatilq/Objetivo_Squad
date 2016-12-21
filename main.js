5f9c20522651e8b956d7a5b931b591da9661cb61
Tatiana Limache Quispe <tati34che@gmail.com>
2016-12-21 07:15:16 -0500

    Ejercicio finalizado sin estilos

---
+++ main.js
@@ -0,0 +1,40 @@
+
+var puntosSquad = document.getElementById("puntos_Squad");
+var maxPuntos = document.getElementById("max_Puntos");
+var divSalida = document.getElementById("salida");
+var porcentaje_Squad = 0;
+
+function calcular()
+{
+	var  valorpuntosSquad=puntosSquad.value;
+	var  valormaxPuntos=maxPuntos.value;
+
+	porcentaje_Squad = (100*parseFloat(puntosSquad.value)) / parseFloat(maxPuntos.value);
+	var valorPorcentajeSquad= parseFloat(porcentaje_Squad);
+
+	if(valorpuntosSquad == "" || valormaxPuntos == "" )
+	{
+
+		salida.innerHTML = "0.00";
+		document.getElementById("mensajes").innerHTML = '<div class="alert alert-danger">No se ingresaron valores</div>';
+	}
+	else if( valorPorcentajeSquad > 100  ||  valorPorcentajeSquad < 0)
+	{
+		salida.innerHTML = "0.00";
+		document.getElementById("mensajes").innerHTML = '<div class="alert alert-danger">Se ingresaron valores incorrectos</div>';
+	}
+	else 
+	{
+		if (valorPorcentajeSquad >= 80 ) 
+		{
+			salida.innerHTML = valorPorcentajeSquad.toFixed(2)+"%";
+			document.getElementById("mensajes").innerHTML = '<div class="alert ">Muy bien Squad</div>';
+		}
+		else
+		{
+			salida.innerHTML = valorPorcentajeSquad.toFixed(2)+"%";
+			document.getElementById("mensajes").innerHTML = '<div class="alert alert-danger">Pueden hacerlo mejor</div>';
+		}
+	}
+
+}
